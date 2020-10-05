873f1740a77f8be20f2c5ed03483f2e4e9acf85c
WestCode725 <wesgoodell@gmail.com>
2020-10-05 17:10:59 -0600

    fix errors

---
+++ lesson4/js/preston.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector('.menubutton');
+const homenav = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {homenav.classList.toggle('responsive')}, false);
