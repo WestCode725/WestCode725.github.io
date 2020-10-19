decf926bcd599b047dddb6c364565a2db5c610a2
WestCode725 <wesgoodell@gmail.com>
2020-10-19 10:21:46 -0600

    fix banner

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -5,4 +5,17 @@ const homenav = document.querySelector('.navigation');
 
 hamburger.addEventListener('click', () => {homenav.classList.toggle('responsive')}, false);
 
+
+let banner = document.querySelector('.banner');
+let day = new Date().getDay();
+
+if (day == 5){
+    banner.style.display = 'block';
+}
+else{
+    banner.style.display = 'none';
+}
+
 })
+
+
