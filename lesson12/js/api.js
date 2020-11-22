39218e4edbb4b924d2ff85e0ec9b201a4c1efdc4
WestCode725 <wesgoodell@gmail.com>
2020-11-22 11:08:28 -0800

    week 10

---
+++ lesson12/js/api.js
@@ -0,0 +1,16 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=1bce53e79e74fa83e4a14612f4b195a1';
+
+fetch(apiURL)
+  .then(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+    const desc = jsObject.weather[0].description; // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
