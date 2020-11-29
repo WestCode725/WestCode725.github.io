c8429cea6a3313dd1be62494fedf32ee52194722
WestCode725 <wesgoodell@gmail.com>
2020-11-28 23:52:34 -0700

    updated preston final

--- lesson13/js/preston-final.js
+++ lesson13/js/preston-final.js
@@ -18,7 +18,20 @@ window.addEventListener('load', (event) => {
         banner.style.display = 'none';
     }
 
+
+
+const prestonURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=1bce53e79e74fa83e4a14612f4b195a1';
+
+fetch(prestonURL)
+    .then(prestonURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject)
+        const days = prestonURL;
+        document.querySelector("currentT").innerHTML = days.main.temp;
+    
     //windchill
+
     let windspeed = parseInt(document.getElementById("windspeed").textContent);
     let airtemp = parseInt(document.getElementById("hightemp").textContent);
     let chill = Math.round((35.74 + (0.6215 * airtemp) - (35 * (windspeed ** 0.16)) + (0.4275 * airtemp * (windspeed ** 0.16))));
@@ -30,3 +43,4 @@ window.addEventListener('load', (event) => {
         document.getElementById("windchill").innerHTML = "N/A";
     }
 });
+})
