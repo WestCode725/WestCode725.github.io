6907c82ed711f615648a4801111d2a942648df9c
WestCode725 <wesgoodell@gmail.com>
2020-12-01 00:09:18 -0700

    update weather

--- lesson13/js/preston-final.js
+++ lesson13/js/preston-final.js
@@ -26,10 +26,11 @@ fetch(prestonURL)
     .then(prestonURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject)
-        const days = prestonURL;
-        document.querySelector("currentT").innerHTML = days.main.temp;
-    
+        document.querySelector("#currentT").innerHTML = Math.round(jsObject.main.temp);
+        document.querySelector("#hightemp").innerHTML = Math.round(jsObject.main.temp_max);
+        document.querySelector("#low").innerHTML = Math.round(jsObject.main.temp_min);
+        document.querySelector("#windspeed").innerHTML = Math.round(jsObject.wind.speed);
+
     //windchill
 
     let windspeed = parseInt(document.getElementById("windspeed").textContent);
@@ -43,4 +44,32 @@ fetch(prestonURL)
         document.getElementById("windchill").innerHTML = "N/A";
     }
 });
+
+
+const fiveday = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=1bce53e79e74fa83e4a14612f4b195a1';
+
+fetch(fiveday)
+.then(fiveday)
+.then((response) => response.json())
+.then((jsObject) => {
+    const list = jsObject['list'];  
+    let x = 1;
+    for (let i = 0; i < list.length; i++){
+        if (list[i].dt_txt.includes('18:00:00')){
+            document.querySelector("#day" + x + "high").textContent = Math.round(list[i].main.temp_max);
+            x++;
+        }
+    }
+})
+
+
+for(let i = 0; i < 7; i++){
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let today = new Date().getDay();
+    let count = today + i;
+    if(count > 6){
+      count -= 7;
+    }
+    document.querySelector('#day'+ (i + 1)).textContent = days[count];
+}
 })
